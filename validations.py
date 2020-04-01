19a8bdd3bca04fc9d3f37b5cbf2c4f4a187bae30
blue-kale2 <63016291+blue-kale2@users.noreply.github.com>
2020-04-01 15:11:26 -0400

    Create validations.py original

---
+++ validations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def vailidate_user(username, minlen):
+    """Checks if the recieved username matches the required conditions."""
+    if type(username) != str:
+        raise TypeError("username must be a string")
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1".)
+        
+        if len(username) < minlen:
+            return False
+        if not username.isalnum():
+            return False
+        # Usernames can't begin with a number
+        if username [0].isnumeric():
+            return False
+        return True
