5686650b363e399ac267fbd1107911c2421dc828
c-bas <47993782+AETHER999@users.noreply.github.com>
2024-03-15 22:49:02 +0100

    Proposed code line
    Add code line 'user validation' as practice.

--- validations.py
+++ validations.py
@@ -7,12 +7,9 @@ def vailidate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1".)
-        
-        if len(username) < minlen:
-            return False
-        if not username.isalnum():
-            return False
-        # Usernames can't begin with a number
+      if len(username) < minlen or not username.isalnum():
+        return False
+     # Usernames can't begin with a number
         if username [0].isnumeric():
             return False
         return True
