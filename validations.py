eabb23f807812c32274b03d47e446016bc0e4958
sebastian <sebastianduke08@yahoo.com>
2024-03-17 19:10:03 +0100

    Fixed typo; a period was deleted on line 9

--- validations.py
+++ validations.py
@@ -6,8 +6,8 @@ def vailidate_user(username, minlen):
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
-        raise ValueError("minlen must be at least 1".)
-        
+        raise ValueError("minlen must be at least 1")
+
         if len(username) < minlen:
             return False
         if not username.isalnum():
