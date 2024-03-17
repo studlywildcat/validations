09f463d5b33177d724dd6485f132fd8caf789640
sebastian <sebastianduke08@yahoo.com>
2024-03-18 00:02:35 +0100

    Fixed conflict on validations.py

--- validations.py
+++ validations.py
@@ -7,7 +7,8 @@ def vailidate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
 
-        raise ValueError("minlen must be at least 1".)
+        raise ValueError("minlen must be at least 1")
+
       if len(username) < minlen or not username.isalnum():
         return False
      # Usernames can't begin with a number
